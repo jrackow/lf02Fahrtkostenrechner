7cbb837ec992fa07ef5bca18996012f27b4850f2
Marvin Krause <marvin.krause2002@gmx.de>
2022-06-27 11:44:25 +0200

    Rechnung funktioniert jetzt

--- Fahrtkostenrechner/Program.cs
+++ Fahrtkostenrechner/Program.cs
@@ -1,29 +1,32 @@
-﻿static void main(string[] args)
-{ 
-    bool temp = false;
+﻿    bool temp = false;
 
     Console.WriteLine("                FahrtKostenrechner");
     Console.WriteLine();
-    while (!temp)
+while (!temp)
+{
+    Console.WriteLine("Bitte geben Sie die geplante Fahrtzeit in Minuten an:");
+
+    int minuten = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Sie wollen " + minuten + " Minuten fahren, korrekt?");
+    Console.WriteLine();
+    Console.WriteLine("Drücken sie 'J' für Ja oder 'N' für Nein");
+
+    var answer = Console.ReadLine();
+    if (answer == "n")
     {
-        Console.WriteLine("Bitte geben Sie die geplante Fahrtzeit in Minuten an:");
-
-        int minuten = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Sie wollen " + minuten + " Minuten fahren, korrekt?");
+        Console.Write("Sie haben 'Nein' gedrückt. Drücken sie eine Taste um eine Erneute Eingabe zu tätigen.");
         Console.WriteLine();
-        Console.WriteLine("Drücken sie 'J' für Ja oder 'N' für Nein");
-
-        var answer = Console.ReadLine();
-        if (answer == "n")
-        {
-            Console.Write("Nein");
-        }
-        else if (answer == "j")
-        {
-            temp = true;
-            Console.WriteLine("Ja Mann");
-        }
-
-        Console.ReadKey();
+        Console.WriteLine();
+        Console.WriteLine();
     }
+    else if (answer == "j")
+    {
+        int kosten = (int)(minuten * 0.25);
+        temp = true;
+        Console.WriteLine("Für eine Fahrt von " + minuten + " zahlen Sie:");
+        Console.WriteLine(kosten + " Euro");
+    }
+
+    Console.ReadKey();
+}
