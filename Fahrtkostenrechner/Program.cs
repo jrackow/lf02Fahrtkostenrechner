43ced1321c6914775e4d678f180a20fb2d771947
Marvin Krause <marvin.krause2002@gmx.de>
2022-06-27 11:10:51 +0200

    Erster Code / Konzept

--- Fahrtkostenrechner/Program.cs
+++ Fahrtkostenrechner/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void main(string[] args)
+{ 
+    bool temp = false;
 
+    Console.WriteLine("                FahrtKostenrechner");
+    Console.WriteLine();
+    while (!temp)
+    {
+        Console.WriteLine("Bitte geben Sie die geplante Fahrtzeit in Minuten an:");
+
+        int minuten = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sie wollen " + minuten + " Minuten fahren, korrekt?");
+        Console.WriteLine();
+        Console.WriteLine("Drücken sie 'J' für Ja oder 'N' für Nein");
+
+        var answer = Console.ReadLine();
+        if (answer == "n")
+        {
+            Console.Write("Nein");
+        }
+        else if (answer == "j")
+        {
+            temp = true;
+            Console.WriteLine("Ja Mann");
+        }
+
+        Console.ReadKey();
+    }
