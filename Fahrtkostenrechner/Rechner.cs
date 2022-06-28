f4782984cb41b72aba7b1d2e0ae5c2a0ab5cf662
Jonathan Lippitsch <jonathan.lippitsch@web.de>
2022-06-28 09:40:41 +0200

    Switch Case nach Abfrage

--- Fahrtkostenrechner/Rechner.cs
+++ Fahrtkostenrechner/Rechner.cs
@@ -8,29 +8,35 @@ public class Rechner
         Console.WriteLine();
         while (continueWhile)
         {
+            eingabe:
             Console.WriteLine("Bitte geben Sie die geplante Fahrtzeit in Minuten an:");
 
             int minuten = Convert.ToInt32(Console.ReadLine());
 
+            abfrage:
             Console.WriteLine("Sie wollen " + minuten + " Minuten fahren, korrekt?");
             Console.WriteLine();
             Console.WriteLine("Drücken sie 'J' für Ja oder 'N' für Nein");
 
             var answer = Console.ReadLine();
-            if (answer == "n")
+
+            switch (answer)
             {
-                Console.Write("Sie haben 'Nein' gedrückt. Drücken sie eine Taste um eine Erneute Eingabe zu tätigen.");
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
+                case "n":
+                    Console.WriteLine("Sie haben 'Nein' gedrückt. Bitte geben sie eine neue Eingabe ein.");
+                    goto eingabe;
+                case "j":
+                    break;
+                default:
+                    Console.WriteLine("Eingabe unbekannt");
+                    goto abfrage;
             }
-            else if (answer == "j")
-            {
-                double kosten = (minuten * 0.25 + 1);
-                continueWhile = false;
-                Console.WriteLine("Für eine Fahrt von " + minuten + " zahlen Sie:");
-                Console.WriteLine(kosten.ToString("0.00") + " Euro");
-            }
+
+            double kosten = (minuten * 0.25 + 1);
+            continueWhile = false;
+            Console.WriteLine("Für eine Fahrt von " + minuten + " zahlen Sie:");
+            Console.WriteLine(kosten.ToString("0.00") + " Euro");
+
             Console.WriteLine();
             Console.WriteLine("Drücken Sie eine Taste, um die Anwendung zu beenden.");
             Console.ReadKey();
