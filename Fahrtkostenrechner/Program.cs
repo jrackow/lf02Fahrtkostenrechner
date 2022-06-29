1ceb46813cad59ad36a6c9f1913f01f60df1d49f
Marvin Krause <marvin.krause2002@gmx.de>
2022-06-29 11:07:25 +0200

    Fenstergröße angepasst

--- Fahrtkostenrechner/Program.cs
+++ Fahrtkostenrechner/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Fahrtkostenrechner;
 
+//Größe der Konsole wird festgelegt 
+Console.SetWindowSize(55, 30);
+
 // Erstellt Objekte für das Management und den Rechner
 Management management = new Management();
 Rechner fahrtkostenrechner = new Rechner(management.Settings);
