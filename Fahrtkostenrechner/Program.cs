e45ef0e7fa528f73047015a28b247acc7a320366
Marvin Krause <marvin.krause2002@gmx.de>
2022-06-29 11:38:59 +0200

    Fenstergröße entfernt

--- Fahrtkostenrechner/Program.cs
+++ Fahrtkostenrechner/Program.cs
@@ -1,14 +1,5 @@
 ﻿using Fahrtkostenrechner;
 
-// Nur auf Windows supported
-try
-{
-    //Größe der Konsole wird festgelegt 
-    Console.SetWindowSize(55, 30);
-}
-catch (System.PlatformNotSupportedException e) {}
-
-
 // Erstellt Objekte für das Management und den Rechner
 Management management = new Management();
 Rechner fahrtkostenrechner = new Rechner(management.Settings);
